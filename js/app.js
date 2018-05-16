7621d1c49049d16d175d273471441e59ac1ee1ed
Chris Kozlowski <kozlowskicd@gmail.com>
2018-05-15 20:51:03 -0700

    add photo, work on css

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Questions about me:
 3. Have I ever been to New York City? -- YES!  I spent a week and a half in New York City as a part of Fleet Week while in the military.
 4. Do I have any pets?  -- NO!  I love animals but do not have a pet at the moment.  My roommates have a dog, 2 cats, and 5 chickens though, so I'm covered on animal companionship right now.
 5. Is is true that my mom I have driven across the country in 2 and a half days? -- YES!  My last duty station was in North Carolina and I needed to get home somehow!
-*/ 
+*/
 
 'use strict';
 
@@ -123,28 +123,28 @@ function questionSix(){
   }
 }
 
-function questionSeven(){
-  var statesLived = ['oregon', 'california', 'idaho', 'north carolina'];
+function questionSeven() {
+  var statesLived = ['oregon', 'california', 'north carolina'];
 
-  var answer7 = prompt('Next, please guess a state I\'ve lived in other than Washington.  You have 6 tries to get one of them.', 'Please type the full state name.').toLowerCase;
+  var answer7 = prompt('Next, please guess a state I\'ve lived in other than Washington.  You have 6 tries to get one of them.', 'Please type the full state name.').toLowerCase();
 
   console.log('Answered: ' + answer7);
 
-  var tries = 0;
+  var tries = 5;
 
-  for (var index = 0; index < statesLived.length; index++) {
-    if (answer7 === statesLived[0]){
+  for (var index = 0; index <= 5; index++) {
+    if (answer7 === statesLived[0] || answer7 === statesLived[1] || answer7 === statesLived[2]){
       points++;
-      alert('That\'s correct!  All of the correct choices are ' + statesLived + '.  +1 point for you!');
+      alert('That\'s correct!  Other than Washington, I\'ve lived in California, Oregon, and North Carolina.');
       break;
     }
     else {
-      tries++;
-      answer7 = prompt('No, that\' not one of them.  ' + user + ', you have ' + (6 - tries) + 'guesses remaining.');
+      tries--;
+      answer7 = prompt('No, that\' not one of them.  ' + user + ', you have ' + (tries + 1) + ' guesses remaining.');
     }
   }
 
-  if (tries === 6) {
+  if (tries === 0) {
     alert('Sorry, out of guesses!');
   }
 }
