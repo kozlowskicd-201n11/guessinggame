6b8e80468c3b73280d78851d225022b66a5dc91a
Chris Kozlowski <kozlowskicd@gmail.com>
2018-05-15 18:37:55 -0700

    cleaning branches

--- js/app.js
+++ js/app.js
@@ -5,16 +5,13 @@
 - console.log messages
 - style with css
 - add, commit, push
-
-
 Questions about me:
-
 3. During introductions on the first day of class, I mentioned that I play a musical instrument.  Was that instrument the electric guitar? -- NO!  Close, but it's the electric bass.
 2. Is my all time favorite movie The Godfather? -- NO!  I like some levity with my movie watching.  My all-time favorite is The Big Lebowski.
 3. Have I ever been to New York City? -- YES!  I spent a week and a half in New York City as a part of Fleet Week while in the military.
 4. Do I have any pets?  -- NO!  I love animals but do not have a pet at the moment.  My roommates have a dog, 2 cats, and 5 chickens though, so I'm covered on animal companionship right now.
 5. Is is true that my mom I have driven across the country in 2 and a half days? -- YES!  My last duty station was in North Carolina and I needed to get home somehow!
-*/ 
+*/
 
 'use strict';
 
@@ -24,148 +21,137 @@ var user = prompt('Hello, welcome the the Chris Kozlowski Guessing Game!  Please
 
 alert('Hello, ' + user + '!  The questions will begin after this alert.  All questions have yes or no answers, so please answer with yes/no or y/n.  Correct answers are worth 1 point each.  Press OK to begin!');
 
-function questionOne(){
-  var answer1 = prompt('During introductions on the first day of class, I mentioned that I play a musical instrument.  Was that instrument the electric guitar?').toLowerCase();
+var answer1 = prompt('During introductions on the first day of class, I mentioned that I play a musical instrument.  Was that instrument the electric guitar?').toLowerCase();
 
-  if (answer1 === 'no' || answer1 === 'n') {
-    alert('Correct!  +1 point');
-    points++;
-  }
-  else {
-    alert('Incorrect :(');
-  }
-
-  alert('Electric guitar is close, but the I play the electric bass.');
-
-  console.log('Question 1: During introductions on the first day of class, I mentioned that I play a musical instrument.  Was that instrument the electric guitar?; Answered: ' + answer1 + '; Cumulative Points: ' + points);
+if (answer1 === 'no' || answer1 === 'n') {
+  alert('Correct!  +1 point');
+  points++;
+}
+else {
+  alert('Incorrect :(');
 }
 
-function questionTwo(){
-  var answer2 = prompt('Is my all time favorite movie The Godfather?').toLowerCase();
+alert('Electric guitar is close, but the I play the electric bass.');
 
-  if (answer2 === 'no' || answer2 === 'n') {
-    alert('Correct!  +1 point');
-    points++;
-  }
-  else {
-    alert('Incorrect :(');
-  }
+console.log('Question 1: During introductions on the first day of class, I mentioned that I play a musical instrument.  Was that instrument the electric guitar?');
+console.log('Answered: ' + answer1);
+console.log('Cumulative Points: ' + points);
 
-  alert('That\'s a good one, but I usually prefer some levity with my movie watching.  My all-time favorite is The Big Lebowski.');
+var answer2 = prompt('Is my all time favorite movie The Godfather?').toLowerCase();
 
-  console.log('Question 2: Is my all time favorite movie The Godfather?; Answered: ' + answer2 + '; Cumulative Points: ' + points);
+if (answer2 === 'no' || answer2 === 'n') {
+  alert('Correct!  +1 point');
+  points++;
+}
+else {
+  alert('Incorrect :(');
 }
 
-function questionThree(){
-  var answer3 = prompt('Have I ever been to New York City?').toLowerCase();
+alert('That\'s a good one, but I usually prefer some levity with my movie watching.  My all-time favorite is The Big Lebowski.');
 
-  if (answer3 === 'yes' || answer3 === 'y') {
-    alert('Correct!  +1 point');
-    points++;
-  }
-  else {
-    alert('Incorrect :(');
-  }
+console.log('Question 2: Is my all time favorite movie The Godfather?');
+console.log('Answered: ' + answer2);
+console.log('Cumulative Points: ' + points);
 
-  alert('I spent a week and a half in New York City as a part of Fleet Week while in the military.');
+var answer3 = prompt('Have I ever been to New York City?').toLowerCase();
 
-  console.log('Question 3: Have I ever been to New York City?; Answered: ' + answer3 + '; Cumulative Points: ' + points);
+if (answer3 === 'yes' || answer3 === 'y') {
+  alert('Correct!  +1 point');
+  points++;
+}
+else {
+  alert('Incorrect :(');
 }
 
-function questionFour(){
-  var answer4 = prompt('Do I have any pets?').toLowerCase();
+alert('I spent a week and a half in New York City as a part of Fleet Week while in the military.');
 
-  if (answer4 === 'no' || answer4 === 'n') {
-    alert('Correct!  +1 point');
-    points++;
-  }
-  else {
-    alert('Incorrect :(');
-  }
+console.log('Question 3: Have I ever been to New York City?');
+console.log('Answered: ' + answer3);
+console.log('Cumulative Points: ' + points);
 
-  alert('I love animals but do not have a pet at the moment.  My roommates have a dog, 2 cats, and 5 chickens though, so I\'m covered on animal companionship right now.');
+var answer4 = prompt('Do I have any pets?').toLowerCase();
 
-  console.log('Question 4: Do I have any pets?; Answered: ' + answer4 + '; Cumulative Points: ' + points);
+if (answer4 === 'no' || answer4 === 'n') {
+  alert('Correct!  +1 point');
+  points++;
+}
+else {
+  alert('Incorrect :(');
 }
 
-function questionFive(){
-  var answer5 = prompt('Is is true that my mom and I have driven across the country?').toLowerCase();
+alert('I love animals but do not have a pet at the moment.  My roommates have a dog, 2 cats, and 5 chickens though, so I\'m covered on animal companionship right now.');
 
-  if (answer5 === 'yes' || answer5 === 'y') {
-    alert('Correct!  +1 point');
-    points++;
-  }
-  else {
-    alert('Incorrect :(');
-  }
+console.log('Question 4: Do I have any pets?');
+console.log('Answered: ' + answer4);
+console.log('Cumulative Points: ' + points);
 
-  alert('My last duty station was in North Carolina and I needed to get home somehow!  We did the trip in only 2 and a half days.');
+var answer5 = prompt('Is is true that my mom and I have driven across the country?').toLowerCase();
 
-  console.log('Question 5: Is is true that my mom and I have driven across the country?; Answered: ' + answer5 + '; Cumulative Points: ' + points);
+if (answer5 === 'yes' || answer5 === 'y') {
+  alert('Correct!  +1 point');
+  points++;
 }
+else {
+  alert('Incorrect :(');
+}
+
+alert('My last duty station was in North Carolina and I needed to get home somehow!  We did the trip in only 2 and a half days.');
+
+console.log('Question 5: Is is true that my mom and I have driven across the country?');
+console.log('Answered: ' + answer5);
+console.log('Cumulative Points: ' + points);
 
 alert('Great job so far, ' + user + '!  Your score so far is ' + points + ' points.  Next up is number guessing!  On the following prompt I will give you a question with a numeric answer.  If you get it correct you will recieve a point, but you will only have 4 tries.  Guess carefully!');
 
-function questionSix(){
-  var answer6 = 33;
-  var attempts = 4;
-  var guess = parseInt(prompt('How old am I?', 'type a number'));
+var answer6 = 33;
+var attempts = 4;
+var guess = parseInt(prompt('How old am I?', 'Guess a number'));
 
+while (attempts >= 1) {
   if (guess > answer6) {
     attempts--;
     guess = parseInt(prompt('Too old, please guess again!  You have ' + attempts + ' attempts remaining.', 'type a number'));
     console.log ('Guess: ' + guess + '; Attempts Remaining: ' + attempts + '; Cumulative Points: ' + points);
-    while (attempts > 1);
   }
-  if (guess < answer6) {
+  else if (guess < answer6) {
     attempts--;
     guess = parseInt(prompt('Too young, please guess again!  You have ' + attempts + ' attempts remaining.', 'type a number'));
     console.log ('Guess: ' + guess + '; Attempts Remaining: ' + attempts + '; Cumulative Points: ' + points);
-    while (attempts > 1);
   }
-  if (attempts === 0 && guess !== answer6) {
-    alert('All out of attampts, dang! The answer was 33.');
-  }
-
-  if (guess === answer6) {
+  else if (guess === answer6) {
     alert('Correct!  +1 point');
     points++;
     console.log ('Guess: ' + guess + '; Attempts Remaining: ' + attempts + '; Cumulative Points: ' + points);
+    break;
   }
 }
 
-function questionSeven(){
-  var statesLived = ['oregon', 'california', 'north carolina'];
+if (attempts === 1 && guess !== answer6) {
+  alert('All out of attampts, dang! The answer was ' + answer6 + '.');
+}
 
-  var answer7 = prompt('Next, please guess a state I\'ve lived in other than Washington.  You have 6 tries to get one of them.', 'Please type the full state name.').toLowerCase();
+var statesLived = ['oregon', 'california', 'north carolina'];
 
-  console.log('Answered: ' + answer7);
+var answer7 = prompt('Next, please guess a state I\'ve lived in other than Washington.  You have 6 tries to get one of them.', 'Please type the full state name.').toLowerCase();
 
-  var tries = 6;
+console.log('Answered: ' + answer7);
 
-  for (var index = 0; index < statesLived.length; index++) {
-    if (answer7 === statesLived[index]){
-      points++;
-      alert('That\'s correct!  The other states are ' + statesLived[1] + ' and ' + statesLived[2]);
-      break;
-    }
-    else {
-      tries--;
-      answer7 = prompt('No, that\' not one of them.  ' + user + ', you have ' + tries + ' guesses remaining.');
-    }
+var tries = 5;
+
+for (var index = 0; index <= 5; index++) {
+  if (answer7 === statesLived[index]) {
+    alert('That\'s correct!  Other than Washington, I\'ve lived in California, Oregon, and North Carolina.');
+    break;
   }
-
-  if (tries === 1) {
-    alert('Sorry, out of guesses!');
+  else {
+    tries--;
+    answer7 = prompt('No, that\' not one of them.  ' + user + ', you have ' + (tries + 1) + ' guesses remaining.');
   }
 }
 
-questionOne();
-questionTwo();
-questionThree();
-questionFour();
-questionFive();
-questionSix();
-questionSeven();
+if (tries === 0) {
+  alert('Sorry, out of guesses!');
+}
 
-console.log('Total points: ' + points);
+alert('This concludes the Chris Kozlowski Guessing Game!  Your final score is: ' + points);
+console.log('Total Points: ' + points);
